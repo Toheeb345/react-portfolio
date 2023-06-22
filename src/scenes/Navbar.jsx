afc38a78cdcbedce729cb44b57a8d9fe348a8a88
Toheeb2023 <omotoshotoheeb2022@gmail.com>
2023-06-21 12:44:13 -1200

    updated

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -17,7 +17,7 @@ const Link = ({ page, selectedPage, setSelectedPage}) => {
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
-    const navbarBackground = isTopOfPage ? "" : "bg-[#090026]";
+    const navbarBackground = isTopOfPage ? "" : "bg-[#120026]";
 
     return (<nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
         <div className="flex items-center justify-between mx-auto w-5/6">
